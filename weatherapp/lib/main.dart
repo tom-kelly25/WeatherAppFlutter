bb998f27bbc2ba3aa3a9cf0927812822efec4765
tom-kelly25 <107217874+tom-kelly25@users.noreply.github.com>
2023-03-25 15:05:41 +0000

    15:05 25/03

--- weatherapp/lib/main.dart
+++ weatherapp/lib/main.dart
@@ -1,9 +1,9 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'models/weather.dart';
-import 'api/weather_api.dart';
-
 import 'package:intl/intl.dart';
+
+import 'api/weather_api.dart';
+import 'models/weather.dart';
+import 'screens/weather_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,79 +18,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: WeatherPage(),
-    );
-  }
-}
-
-class WeatherPage extends StatefulWidget {
-  @override
-  _WeatherPageState createState() => _WeatherPageState();
-}
-
-class _WeatherPageState extends State<WeatherPage> {
-  final _cityController = TextEditingController();
-  Weather? _weather;
-
-  Future<void> _getWeatherData() async {
-    final city = _cityController.text.trim();
-    if (city.isNotEmpty) {
-      final weather = await WeatherApi.getCurrentWeather(city);
-      setState(() {
-        _weather = weather;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Weather App'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _cityController,
-              decoration: InputDecoration(
-                labelText: 'Enter city name',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: _getWeatherData,
-              child: Text('Get Weather'),
-            ),
-            SizedBox(height: 16.0),
-            if (_weather != null) ...[
-              Text(
-                _weather!.city,
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                '${_weather!.temperature.toStringAsFixed(1)}°C',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                _weather!.description,
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                'Last updated: ${DateFormat.yMd().add_jm().format(DateTime.now())}',
-                style: TextStyle(fontSize: 12.0),
-              ),
-            ],
-          ],
-        ),
-      ),
     );
   }
 }
